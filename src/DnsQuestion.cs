95c15a5736b2f51c69817dd5954e3d844540e678
Aleksander Kuś <79520681+alexanderKus@users.noreply.github.com>
2025-02-01 12:01:54 +0100

    Task2Fix4

--- src/DnsQuestion.cs
+++ src/DnsQuestion.cs
@@ -1,3 +1,5 @@
+using System.Buffers.Binary;
+
 namespace codecrafters_dns_server;
 
 public class DnsQuestion(DnsDomain name, ushort type, ushort cls)
@@ -9,10 +11,8 @@ public class DnsQuestion(DnsDomain name, ushort type, ushort cls)
     public int Write(Span<byte> buffer)
     {
         var nameLength = Name.Write(buffer);
-        buffer[nameLength + 1] = (byte)(Type >> 8);
-        buffer[nameLength + 2] = (byte)Type;
-        buffer[nameLength + 3] = (byte)(Class >> 8);
-        buffer[nameLength + 4] = (byte)Class;
+        BinaryPrimitives.WriteUInt16BigEndian(buffer[(nameLength + 1)..], Type);
+        BinaryPrimitives.WriteUInt16BigEndian(buffer[(nameLength + 3)..], Class);
         return nameLength + 4;
     }
 }
